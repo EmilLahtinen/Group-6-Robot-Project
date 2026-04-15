14d0a93ccd84fbc1b3ff48a707a7847e77ed9b6d
imalkaperera423-rgb <imalkaperera423@gmail.com>
2026-04-15 13:25:24 +0300

    written MotorTest.java for clarity
    this code is for test purpose only

--- RobotProject/src/MotorTest.java
+++ RobotProject/src/MotorTest.java
@@ -1,3 +1,4 @@
+//this code is written for test purpose only
 package src;
 
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
@@ -6,23 +7,24 @@ import lejos.utility.Delay;
 
 public class MotorTest {
     public static void main(String[] args) {
-        // creating motor objects
-        EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.C);
-        EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.D);
+        //  Create motor objects and connect them to EV3 ports
+        EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.C); //MotorPort.C = motor connected to port C
+        EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.D);//MotorPort.D = motor connected to port D
 
-        System.out.println("motor test starting...");
+        System.out.println("motor test starting..."); 
 
-        // move motors forward
+        // Set speed for both motors
+        //move motors forward
         leftMotor.setSpeed(360);   // degrees per second
         rightMotor.setSpeed(360);
         leftMotor.forward();
         rightMotor.forward();
-        Delay.msDelay(2000);       // 2 secs
+        Delay.msDelay(2000);       //  Keep moving forward for 2 seconds
 
         // Move motors backward
         leftMotor.backward();
         rightMotor.backward();
-        Delay.msDelay(2000);       
+        Delay.msDelay(2000);       //Keep moving backward for 2 seconds 
 
         // Stop motors
         leftMotor.stop();
@@ -32,6 +34,6 @@ public class MotorTest {
         leftMotor.close();
         rightMotor.close();
 
-        System.out.println("test completed. Motors shoulve been working");
+        System.out.println("test completed. Motors shoulve been working"); //final message after test completes
     }
 }
