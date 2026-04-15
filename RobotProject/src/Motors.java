d91342e5c4249bb3b8302738f089afcf9d18329a
imalkaperera423-rgb <imalkaperera423@gmail.com>
2026-04-15 13:45:10 +0300

    written and enhance the logic? Motors.java
    having some arguements of the logic but it works

--- RobotProject/src/Motors.java
+++ RobotProject/src/Motors.java
@@ -5,15 +5,20 @@ import lejos.hardware.port.MotorPort;
 
 public class Motors {
     
-    EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.C);
+    EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.C); //Create motor objects connected to EV3 ports
     EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.D);
 
+    //Move both motors forward
     public void forward(){
         leftMotor.setSpeed(360);   // degrees per second
         rightMotor.setSpeed(360);
+     
+    //start spinning forward   
         leftMotor.forward();
         rightMotor.forward();
     }
+   
+    //stop both motors immidiatly
     public void stop(){
         leftMotor.stop();
         rightMotor.stop();
