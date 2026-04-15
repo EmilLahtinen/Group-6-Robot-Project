6c75ec7d50d67dfd22a78e9c4c3dac750f2bdcef
imalkaperera423-rgb <imalkaperera423@gmail.com>
2026-04-15 12:58:20 +0300

    Enhance LightSensor.java

--- RobotProject/src/LightSensor.java
+++ RobotProject/src/LightSensor.java
@@ -10,8 +10,8 @@ public class LightSensor {
 
     public static void main(String[] args)
     {
-        EV3ColorSensor colorSensor  = new EV3ColorSensor(SensorPort.S3);
-        SampleProvider light        = colorSensor.getAmbientMode();
+        EV3ColorSensor colorSensor  = new EV3ColorSensor(SensorPort.S3); //  Create color sensor object on EV3 sensor port S3
+        SampleProvider light        = colorSensor.getAmbientMode(); //Set sensor mode to measure ambient light (general brightness)
         
         // Create an array to hold the sensor data
         float[] sample = new float[light.sampleSize()];
@@ -26,7 +26,8 @@ public class LightSensor {
             // Display the light intensity value on the LCD screen
             LCD.clear();
             LCD.drawString("Light Intensity: " + (int)(sample[0] * 100) + "%", 0, 0);  // Display as percentage
-            
+
+            //Small delay so values don’t update too fast
             try 
             {
                 Thread.sleep(100);
