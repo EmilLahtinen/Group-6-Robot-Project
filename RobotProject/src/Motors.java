1b3f4f5e5a8df6da640513fefc8de2caf67adcad
imalkaperera423-rgb <imalkaperera423@gmail.com>
2026-04-29 09:35:19 +0300

    Refactor Motors.java with improved comments
    Updated comments for clarity and maintained functionality.

--- RobotProject/src/Motors.java
+++ RobotProject/src/Motors.java
@@ -5,13 +5,13 @@ import lejos.hardware.port.MotorPort;
 
 public class Motors {
     
-    EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.C);
+    EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.C);//create motors
     EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.D);
-    double baseSpeed = 270;
-    double correction = 0;
+    double baseSpeed = 270;//create distance both motors 270 is enough speed
+    double correction = 0;//this gonna use for pid system
 
     public void forward(){
-        leftMotor.setSpeed(360);   // Set to 360 for real deal
+        leftMotor.setSpeed(360);   // Set to 360 for real deal //these were used before PID now it used for obstacle avoidence
         rightMotor.setSpeed(360);   // Set to 360 for real deal
         leftMotor.forward();
         rightMotor.forward();
@@ -30,28 +30,28 @@ public class Motors {
         leftMotor.setSpeed(90);
         rightMotor.setSpeed(90);
         rightMotor.backward();
-        leftMotor.forward();
+        leftMotor.forward(); 
     }
     public void turnLeft(){
-        rightMotor.setSpeed(360);   // Set to 360 for real deal
+        rightMotor.setSpeed(360);   // Set to 360 for real deal // we dont use these any more it used before we implemnt the PID
         leftMotor.setSpeed(135);    // Set to 135 for real deal
         rightMotor.forward();
         leftMotor.forward();
     }
     public void turnRight(){
-        leftMotor.setSpeed(360);    // Set to 360 for real deal
+        leftMotor.setSpeed(360);    // Set to 360 for real deal // we dont use it now
         rightMotor.setSpeed(135);   // Set to 135 for real deal
         rightMotor.forward();
         leftMotor.forward();
     }
     public void pidDrive(){
-        double leftSpeed = baseSpeed + correction;
+        double leftSpeed = baseSpeed + correction; //this part is done by both imalka and emil together
         double rightSpeed = baseSpeed - correction;
 
         leftSpeed = Math.max(0, Math.min(360, leftSpeed));
         rightSpeed = Math.max(0, Math.min(360, rightSpeed));
         
-        leftMotor.setSpeed((int)leftSpeed);
+        leftMotor.setSpeed((int)leftSpeed); //this requires int value it gives errors 
         rightMotor.setSpeed((int)rightSpeed);
         leftMotor.forward();
         rightMotor.forward();
